59b5a2dba5307a232d405f493ea15bd924b642a2
syf <2654525303@qq.com>
2024-11-08 23:43:36 +0800

    #5 Instantiating New Scenes

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -5,17 +5,42 @@ namespace PuzzleGameCourse;
 public partial class Main : Node2D
 {
     private Sprite2D _sprite;
+    private PackedScene _buildingScene;
+
     public override void _Ready()
     {
+        _buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
         _sprite = GetNode<Sprite2D>("Cursor");
     }
 
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event.IsActionPressed("left_click"))
+        {
+            PlaceBuildingAtMousePosition();
+        }
+    }
+
     public override void _Process(double delta)
+    {
+        _sprite.GlobalPosition = GetMouseGridCellPosition() * 64;
+    }
+
+    private Vector2 GetMouseGridCellPosition()
     {
         var mousePosition = GetGlobalMousePosition();
         var gridPosition = mousePosition / 64;
         gridPosition = gridPosition.Floor();
 
-        _sprite.GlobalPosition = gridPosition * 64;
+        return gridPosition;
+    }
+
+    private void PlaceBuildingAtMousePosition()
+    {
+        var building = _buildingScene.Instantiate<Node2D>();
+        AddChild(building);
+        
+        var gridPosition = GetMouseGridCellPosition();
+        building.GlobalPosition = gridPosition * 64;
     }
 }
