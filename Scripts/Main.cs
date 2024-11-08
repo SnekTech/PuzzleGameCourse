9cb4883bf99cb598f1e8617c126b67733a514114
syf <2654525303@qq.com>
2024-11-08 22:35:39 +0800

    #4 The First C# Script

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -4,14 +4,18 @@ namespace PuzzleGameCourse;
 
 public partial class Main : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("hello godot");
-	}
+    private Sprite2D _sprite;
+    public override void _Ready()
+    {
+        _sprite = GetNode<Sprite2D>("Cursor");
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Process(double delta)
+    {
+        var mousePosition = GetGlobalMousePosition();
+        var gridPosition = mousePosition / 64;
+        gridPosition = gridPosition.Floor();
+
+        _sprite.GlobalPosition = gridPosition * 64;
+    }
 }
