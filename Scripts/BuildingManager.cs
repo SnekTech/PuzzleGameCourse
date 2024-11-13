d170d48800635dfef4c5f5a30dc16e86863d590e
syf <2654525303@qq.com>
2024-11-13 19:22:56 +0800

    #28 Creating A Building Manager

---
+++ Scripts/BuildingManager.cs
@@ -0,0 +1,92 @@
+﻿using Godot;
+using PuzzleGameCourse.Building;
+using PuzzleGameCourse.UI;
+
+namespace PuzzleGameCourse;
+
+public partial class BuildingManager : Node
+{
+    [Export]
+    private GridManager gridManager;
+
+    [Export]
+    private GameUI gameUI;
+
+    [Export]
+    private Node2D ySortRoot;
+
+    [Export]
+    private Sprite2D cursor;
+
+    private int _currentResourceCount;
+    private int _startingResourceCount = 4;
+    private int _currentlyUsedResourceCount;
+    private BuildingResource _toPlaceBuildingResource;
+    private Vector2I? _hoveredGridCell;
+
+    private int AvailableResourceCount => _startingResourceCount + _currentResourceCount - _currentlyUsedResourceCount;
+
+    public override void _Ready()
+    {
+        gridManager.ResourceTilesUpdated += OnResourceTilesUpdated;
+        gameUI.BuildingResourceSelected += OnBuildingResourceSelected;
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (_hoveredGridCell.HasValue &&
+            _toPlaceBuildingResource != null &&
+            @event.IsActionPressed("left_click") &&
+            gridManager.IsTilePositionBuildable(_hoveredGridCell.Value) &&
+            AvailableResourceCount >= _toPlaceBuildingResource.ResourceCost)
+        {
+            PlaceBuildingAtHoveredCellPosition();
+            cursor.Visible = false;
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        var gridPosition = gridManager.GetMouseGridCellPosition();
+        cursor.GlobalPosition = gridPosition * 64;
+
+        if (_toPlaceBuildingResource != null && cursor.Visible &&
+            (!_hoveredGridCell.HasValue || gridPosition != _hoveredGridCell.Value))
+        {
+            _hoveredGridCell = gridPosition;
+            gridManager.ClearHighlightedTiles();
+            gridManager.HighlightExpandedBuildableTiles(_hoveredGridCell.Value,
+                _toPlaceBuildingResource.BuildableRadius);
+            gridManager.HighlightResourceTiles(_hoveredGridCell.Value, _toPlaceBuildingResource.ResourceRadius);
+        }
+    }
+
+    private void PlaceBuildingAtHoveredCellPosition()
+    {
+        if (!_hoveredGridCell.HasValue)
+            return;
+
+        var building = _toPlaceBuildingResource.BuildingScene.Instantiate<Node2D>();
+        ySortRoot.AddChild(building);
+
+        building.GlobalPosition = _hoveredGridCell.Value * 64;
+
+        _hoveredGridCell = null;
+        gridManager.ClearHighlightedTiles();
+
+        _currentlyUsedResourceCount += _toPlaceBuildingResource.ResourceCost;
+        GD.Print(AvailableResourceCount);
+    }
+
+    private void OnResourceTilesUpdated(int resourceCount)
+    {
+        _currentResourceCount = resourceCount;
+    }
+
+    private void OnBuildingResourceSelected(BuildingResource buildingResource)
+    {
+        _toPlaceBuildingResource = buildingResource;
+        cursor.Visible = true;
+        gridManager.HighLightBuildableTiles();
+    }
+}
--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,81 +1,10 @@
 using Godot;
-using PuzzleGameCourse.Building;
-using PuzzleGameCourse.UI;
 
 namespace PuzzleGameCourse;
 
 public partial class Main : Node
 {
-    private GridManager _gridManager;
-    private Sprite2D _cursor;
-    private Node2D _ySortRoot;
-    private GameUI _gameUI;
-
-    private Vector2I? _hoveredGridCell;
-    private BuildingResource _toPlaceBuildingResource;
-
     public override void _Ready()
     {
-        _gridManager = GetNode<GridManager>("GridManager");
-        _cursor = GetNode<Sprite2D>("Cursor");
-        _ySortRoot = GetNode<Node2D>("YSortRoot");
-        _gameUI = GetNode<GameUI>("GameUI");
-
-        _cursor.Visible = false;
-
-        _gameUI.BuildingResourceSelected += OnBuildingResourceSelected;
-        _gridManager.ResourceTilesUpdated += OnResourceTilesUpdated;
-    }
-
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (_hoveredGridCell.HasValue && @event.IsActionPressed("left_click") &&
-            _gridManager.IsTilePositionBuildable(_hoveredGridCell.Value))
-        {
-            PlaceBuildingAtHoveredCellPosition();
-            _cursor.Visible = false;
-        }
-    }
-
-    public override void _Process(double delta)
-    {
-        var gridPosition = _gridManager.GetMouseGridCellPosition();
-        _cursor.GlobalPosition = gridPosition * 64;
-
-        if (_toPlaceBuildingResource != null && _cursor.Visible &&
-            (!_hoveredGridCell.HasValue || gridPosition != _hoveredGridCell.Value))
-        {
-            _hoveredGridCell = gridPosition;
-            _gridManager.ClearHighlightedTiles();
-            _gridManager.HighlightExpandedBuildableTiles(_hoveredGridCell.Value,
-                _toPlaceBuildingResource.BuildableRadius);
-            _gridManager.HighlightResourceTiles(_hoveredGridCell.Value, _toPlaceBuildingResource.ResourceRadius);
-        }
-    }
-
-    private void PlaceBuildingAtHoveredCellPosition()
-    {
-        if (!_hoveredGridCell.HasValue)
-            return;
-
-        var building = _toPlaceBuildingResource.BuildingScene.Instantiate<Node2D>();
-        _ySortRoot.AddChild(building);
-
-        building.GlobalPosition = _hoveredGridCell.Value * 64;
-
-        _hoveredGridCell = null;
-        _gridManager.ClearHighlightedTiles();
-    }
-
-    private void OnBuildingResourceSelected(BuildingResource buildingResource)
-    {
-        _toPlaceBuildingResource = buildingResource;
-        _cursor.Visible = true;
-        _gridManager.HighLightBuildableTiles();
-    }
-
-    private void OnResourceTilesUpdated(int resourceCount)
-    {
-        GD.Print(resourceCount);
     }
 }
