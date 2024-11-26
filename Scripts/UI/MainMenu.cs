fcfa0ec625235abfe1c9c722b394873ebffada9e
syf <2654525303@qq.com>
2024-11-26 21:13:29 +0800

    #57 Building a Framework for Changing Levels

---
+++ Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using Godot;
+using PuzzleGameCourse.Autoload;
+
+namespace PuzzleGameCourse.UI;
+
+public partial class MainMenu: Node
+{
+    private Button playButton;
+
+    public override void _Ready()
+    {
+        playButton = GetNode<Button>("%PlayButton");
+
+        playButton.Pressed += OnPlayButtonPressed;
+    }
+
+    private void OnPlayButtonPressed()
+    {
+        LevelManager.Instance.ChangeToLevel(0);
+    }
+}
