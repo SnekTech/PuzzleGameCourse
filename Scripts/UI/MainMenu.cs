1e2b40c5b29e5e093a96904c92e576c96a75c314
syf <2654525303@qq.com>
2024-11-28 21:57:26 +0800

    #65 Integrating the Level Select Screen with the Main Menu

--- Scripts/UI/MainMenu.cs
+++ Scripts/UI/MainMenu.cs
@@ -1,21 +1,43 @@
 ﻿using Godot;
-using PuzzleGameCourse.Autoload;
 
 namespace PuzzleGameCourse.UI;
 
 public partial class MainMenu: Node
 {
     private Button playButton;
+    private Button quitButton;
+    private Control mainMenuContainer;
+    private LevelSelectScreen levelSelectScreen;
 
     public override void _Ready()
     {
         playButton = GetNode<Button>("%PlayButton");
+        quitButton = GetNode<Button>("%QuitButton");
+        mainMenuContainer = GetNode<Control>("%MainMenuContainer");
+        levelSelectScreen = GetNode<LevelSelectScreen>("%LevelSelectScreen");
+
+        mainMenuContainer.Visible = true;
+        levelSelectScreen.Visible = false;
 
         playButton.Pressed += OnPlayButtonPressed;
+        levelSelectScreen.BackPressed += OnLevelSelectBackPressed;
+        quitButton.Pressed += OnQuitButtonPressed;
     }
 
     private void OnPlayButtonPressed()
     {
-        LevelManager.Instance.ChangeToLevel(0);
+        mainMenuContainer.Visible = false;
+        levelSelectScreen.Visible = true;
+    }
+
+    private void OnLevelSelectBackPressed()
+    {
+        mainMenuContainer.Visible = true;
+        levelSelectScreen.Visible = false;
+    }
+
+    private void OnQuitButtonPressed()
+    {
+        GetTree().Quit();
     }
 }
